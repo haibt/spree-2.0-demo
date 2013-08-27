7fee987c00dbd226eebff69f20815bd852025870
Bui Thanh Hai <hai.bth@gmail.com>
2013-08-27 11:27:57 +0700

    Init the Spree webstore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpreeDemo::Application.config.secret_key_base = '30a4db13d57508c8000ef05021ace9c524587a939b5be36e42f5793350c99b8a65be0a14d9cf7dea2ae300677ad987c3100d28ba54142d50cd2974040c990287'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SpreeDemo::Application.config.session_store :cookie_store, key: '_scrum2b_spree_2_demo_session'
